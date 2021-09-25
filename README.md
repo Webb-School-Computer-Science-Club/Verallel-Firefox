a1851a48ffb6a56d589c4b650c0bf766b10725ad
tate <76761406+tatereed@users.noreply.github.com>
2021-09-25 07:11:22 -0400

    3 Content Scripts working
    I am not entirely sure what was going on. I used three content scripts in order to do the same thing as 'background.js' in the main Verallel extension since background scripts are deprecated on Firefox. It works, but anything that needs to be added to background.js will likely have to be made into either a content script or using another method like tabs.executeScript()

--- README.md
+++ README.md
@@ -1,28 +1,34 @@
-<a href='https://github.com/Webb-School-Computer-Science-Club/Verallel'>Check out the Chrome version!</a><br>
-This release is currently updated to the beta v1.0.5  of the chrome version. It will be released onto the Firefox Addons Store along with the release of the Chrome v1.1.0
+<a href='https://chrome.google.com/webstore/detail/verallel/gndjmpiohedkdigkpibefbaoboajlnah'>Get it on the chrome webstore</a><br>
+This release is version 1.0.0, so the only feature it has is clickable links to the dropbox from the classpage. Any commits after the latest release are for version 1.1.0, which will hopefuly be a big update. A version 1.0.5 has released, but that is a beta version for some feature testing. Version 1.1.0 will be the next version on the web store, not version 1.0.5.
 
-# Verallel-Firefox
-A student-made, student-tailored Firefox addon for Veracross. Provides ease of navigation in Veracross, with other quality of life features, and even more in progress.<br>
-Currently, the only features of the extension are to add a link to the dropbox from any class popup in the class tab and to remove the mess of numbers and letters before the name of the actual class in the class tab, but other featues are going to come soon!<br>
+# Verallel
+A student-made, student-tailored Chrome web extension for Veracross. Provides ease of navigation in Veracross, with other quality of life features, and even more in progress.<br>
+Currently, the only features of 1.0.0 are to add a link to the dropbox from any class popup in the class tab, but other featues are going to come soon in the 1.1.0 release!<br>
 Unfortunatley, at the moment, this extension is compatible only for Webb students, as the links will not work if your Veracross is a different school. 
 
 # Jump to
+[Upcoming Updates](#upcoming-updates)
+
 [Issue Handling](#issue-handling)
 
 [Helping with Development](#helping-with-development)
 
 [Potential Feature Additions](#potential-feature-addons-the-dev-team-has-high-on-the-bucket-list)
 
+# Upcoming Updates
+Compared to version 1.0.0, version 1.1.0 has these following upgrades (keep in mind this list is going to get bigger as 1.1.0 hasn't been fully developed yet):
+  - Fetching all upcoming assignments from popup page, putting assignments due the day of any the day after first
+  - Link to dropbox and general cleanup of assignment detail page in portals.veracross.com, along with improvement of dropbox page UI
 # Issue Handling
 When it comes to repository issues, please keep in mind that the developer team for Verallel has an order of priorities for issue fixing. There isn't a rigid order, but expect issues about major bugs to be solved first over adding minor feature enhancements. This shouldn't deter you from making an issue for a bug or a feature request if you have one, but acknowledge that there a limited amount of people on the team, each with other commitments of their own. <br> If you make an issue about a bug, please detail how you got to the bug and if that bug is consistently reproducible. Additionally, make sure your issue is not already an issue on the repository. 
 
 # Helping with Development
-If you wish to help with the development of the site, the zip folder release of the extension will be provided (and maybe some other beta versions the dev team is working on). If you are not part of the dev team and end up making your own working version please publish the code as a fork of this repository.
+If you wish to help with the development of the site, the zip folder release of the extension will be provided (and maybe some other beta versions the dev team is working on). If you are not part of the dev team and end up making yor own working version please publish the code as a fork of this repository. Additionally, make sure to detail the changes you make in your fork compared to the original.<br>
+This is the repository for the Chrome extension for Verallel. If you want to help with Verallel as a Firefox add-on, you should check out <a href="https://github.com/Webb-School-Computer-Science-Club/Verallel-Firefox">Verallel-Firefox</a>
 
 # Potential Feature Additions the Dev Team has High on the Bucket List
-  - The popup window being a dark/light mode toggler for Veracross as well as the popup window (adding some more purpose to the big button in the popup window)
-  - Links to Dropbox from other assignment submission
+  - The popup window having a dark/light mode toggler for Veracross
+  - Links to Dropbox from other places that show assignments
   - Making the links compatible for other schools the most conveniently
-  - Making it multi-browser compatible
-  - Assignment notifications
+  - Cleaning up class ids and classes in places such as main student portal
 
--- popup.js
+++ popup.js
@@ -1,29 +1,29 @@
 // popup.js
 // Does notification button for assignments
 // One minor bug: \" in assignments doesn't get replaced by "
-// May or may not need to sort by year, but that will probably be next update for getAssignments()
+// May or may not need to sort by year, but that will probably be an upcoming update for getAssignments()
 
-document.getElementById("notif").addEventListener("click", reqNotify);
+document.getElementById("notif").addEventListener("click", reqNotify); // Gives button functionality by adding click listener
 var assign = false;
 const monthdict = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}; //3-letter month to number conversion
 
-async function getAssignments(){
-    const respNew = await fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments');
-    const txt = await respNew.text();
-    const stuId = txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=')[txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=').length - 1];
-    const yr = txt.split(';')[16].split('"')[0].split('=')[1];
-    const fetchUrl = 'https://portals-embed.veracross.com/webb/parent/planner?p=' + stuId + '&school_year=' + yr;
-    const response = await fetch(fetchUrl);
+async function getAssignments(){ // async for usage of fetch
+    const respNew = await fetch('https://portals.veracross.com/webb/student/student/upcoming-assignments'); // Link not unique to student
+    const txt = await respNew.text(); // Trying to get json doesn't work unfortunatley
+    const stuId = txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=')[txt.split(';')[15].split('&')[txt.split(';')[15].split('&').length - 2].split('=').length - 1]; // Getting student id out of raw text of source code
+    const yr = txt.split(';')[16].split('"')[0].split('=')[1]; // Gets the year from the raw text of the source code
+    const fetchUrl = 'https://portals-embed.veracross.com/webb/parent/planner?p=' + stuId + '&school_year=' + yr; // Configures student-unique url to fetch the iframe (can't fetch in original because of CORS)
+    const response = await fetch(fetchUrl); //New fetching
     const fg = await response.text();
-    let today = new Date();
+    let today = new Date(); //Defining today and tomorrow
     let tommorow = new Date(today);
     tommorow.setDate(tommorow.getDate() + 1);
-    const fh = ((fg.split(';')[15]).split('},{'));
-    var assignments = [];
+    const fh = ((fg.split(';')[15]).split('},{')); // More text splitting to retrieve features
+    var assignments = []; // Defining arrays and objects for use in the function
     var classRows = {};
     var assignDate = {};
-    var isOn = false;
-    for (let i = 0; i < fh.length; i++){
+    var isOn = false; // Mostly for making sure no assignment repeats and no assignments before current date are on there
+    for (let i = 0; i < fh.length; i++){ // Loops through all possible assignments
         var isToday = false;
         var isTmrw = false;
 	    if (fh[i].split(',')[2].split(':')[0] == '"class_id"'){
@@ -35,7 +35,36 @@ async function getAssignments(){
             var assignStr = '';
             let commaMuch = fh[i].split(',').length - 11;
             if (commaMuch <= 0){
-                assignStr =  'Due ' +  dueDate + ' - ' + (fh[i].split(',')[7]).split(':')[1].slice(1, (fh[i].split(',')[7]).split(':')[1].length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ')';
+                let colonMuch = fh[i].split(',')[7].split(':').length;
+                if (colonMuch <= 2){ // Checking if the assignment has a colon in it
+                    assignStr =  'Due ' +  dueDate + ' - ' + (fh[i].split(',')[7]).split(':')[1].slice(1, (fh[i].split(',')[7]).split(':')[1].length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ')';
+                }
+                else{
+                    for(let o = 0; o <= colonMuch - 2; o++){
+			let colonMuch = fh[i].split(',')[7 + o].split(':').length; // For every comma-separated item colon spliiting is now addressed
+                        if(o == 0){
+                            if(colonMuch <= 2){
+                                assignStr = assignStr + fh[i].split(',')[7].split(':')[1];
+                            }
+                            else{
+                                for(let n = 0; n <= colonMuch - 2; n++){
+                                    if(n == 0){
+                                        assignStr = assignStr + fh[i].split(',')[7].split(':')[1];
+                                    }
+                                    else{
+                                        assignStr = assignStr + ': ';
+                                        assignStr = assignStr + fh[i].split(',')[7].split(':')[1 + n];
+                                    }
+                                }
+                            }
+                        }
+                        else{
+                            assignStr = assignStr + ': ';
+                            assignStr = assignStr + fh[i].split(',')[7].split(':')[1 + o];
+                        }
+                    }
+                    assignStr = 'Due ' + dueDate + ' - ' + assignStr.slice(1, assignStr.length - 1).replace('\\', '') + ' (' + classRows[fh[i].split(',')[1].split(':')[1]].replace('"', '').replace('"', '') + ')';
+                }
             }
             else{
                 for (let o = 0; o <= commaMuch; o++){
@@ -53,7 +82,7 @@ async function getAssignments(){
                 isOn = true;
             }
             else{
-                if(dueDatelis[1] < today.getDate()){
+                if((dueDatelis[1] < today.getDate()) && (dueDatelis[0] == today.getMonth() + 1)){
                     isOn = true;
                 }
                 else if(dueDatelis[1] == today.getDate()  && dueDatelis[0] == today.getMonth() + 1){ // If the assignment is due the day of
@@ -85,7 +114,7 @@ async function getAssignments(){
 		    }
 		}
 	    }
-            if(!isOn){ //The assignment should only appear once
+            if(!isOn){ // Adding elements to assignDate[today], assignDate[tmrw], or assignments if need be
                 if(!isToday){
                     if(isTmrw){
                         if(assignDate['tmrw'] == null){
@@ -108,7 +137,7 @@ async function getAssignments(){
                     }
                 }
             }
-	    isOn = false;
+	    isOn = false; // Resets every iteration
         }
     }
     var g = document.getElementById('assign'); // Getting the empty div in popup.html
@@ -116,14 +145,14 @@ async function getAssignments(){
     pinit.style = "font-size: 150%; text-align: center;";
     pinit.appendChild(document.createTextNode('Here are your upcoming assignments:'));
     g.appendChild(pinit);
-    if(!(assignDate['today']==null)){
-        for (let k = 0; k < assignDate['today'].length; k++){ // Assignments due today
+    if(!(assignDate['today'] == null)){ // Needs to exist if the length attribute can be collected from it
+    	for (let k = 0; k < assignDate['today'].length; k++){ // Assignments due today
             var h = document.createElement('p');
             h.appendChild(document.createTextNode(assignDate['today'][k]));
             g.appendChild(h);
         }
     }
-    if(!(assignDate['tmrw']==null)){
+    if(!(assignDate['tmrw'] == null)){ // Needs to exist if the length attribute can be collected from it
         for (let k = 0; k < assignDate['tmrw'].length; k++){ // Assignments due tommoroow
             var h = document.createElement('p');
             h.appendChild(document.createTextNode(assignDate['tmrw'][k]));
@@ -139,7 +168,7 @@ async function getAssignments(){
 	    h.appendChild(document.createTextNode(assignments[k]));
 	    g.appendChild(h);
     }
-    var pf = document.createElement('p')
+    var pf = document.createElement('p'); // Adding ending link and p tag
     pf.appendChild(document.createTextNode('If you want more info, visit '));
     var a = document.createElement('a');
     a.href = "https://portals.veracross.com/webb/student/student/upcoming-assignments";
@@ -151,7 +180,7 @@ async function getAssignments(){
 }
 
 
-function reqNotify(){
+function reqNotify(){ // To make sure getAssignments doesn't run more than once
     if (!(assign)){
 	getAssignments();
         assign = true;
